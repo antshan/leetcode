a1c76ddbda8543879fea8c5de7b70a23d90d833e
shanwei <shanwei0949@163.com>
2019-01-08 21:12:48 +0800

    771-1

--- easyAlgorithm/src/jewelsInStones.java
+++ easyAlgorithm/src/jewelsInStones.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 771. 宝石与石头
  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
@@ -24,6 +27,8 @@
 public class jewelsInStones {
 
     class Solution {
+
+        //方法1
         public int numJewelsInStones(String J, String S) {
 
             char[] jew= J.toCharArray();
@@ -40,11 +45,29 @@ public class jewelsInStones {
 
             return sum;
         }
+
+        //方法2
+        public int numJewelsInStones1(String J,String S){
+            Map<Character,Integer> map= new HashMap<>();
+            int sum=0;
+            for(char chj:J.toCharArray()){
+                map.put(chj,0);
+            }
+            for (char chs:S.toCharArray()){
+                if (map.containsKey(chs)){
+                    map.put(chs,map.get(chs)+1);
+                }
+            }
+            for (Map.Entry<Character,Integer> mm:map.entrySet()){
+              sum+=mm.getValue();
+            }
+            return sum;
+        }
     }
 
     public static void main(String[] args){
         Solution s = new jewelsInStones().new Solution();
-        int sum =s.numJewelsInStones("z","ZZ");
+        int sum =s.numJewelsInStones1("aA","aAAbbbb");
         System.out.println("======sum:"+sum+"=======");
     }
 }
