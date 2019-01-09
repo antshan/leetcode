20b72fc8e3677ed51c9f42247dd47f810281c774
shanwei <shanwei0949@163.com>
2019-01-09 13:33:35 +0800

    965

---
+++ easyAlgorithm/src/UnivaluedBinaryTree.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 965 单值二叉树
+ * 如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树。
+
+ 只有给定的树是单值二叉树时，才返回 true；否则返回 false。
+ */
+public class UnivaluedBinaryTree {
+
+    List<Integer> vlas = new ArrayList<>();
+    public boolean isUnivalTree(TreeNode root) {
+
+        dfs(root);
+        for (int v:vlas){
+            if (v!=vlas.get(0))
+                return false;
+        }
+        return true;
+    }
+
+    public void dfs(TreeNode treeNode){
+        if (treeNode!=null){
+            vlas.add(treeNode.val);
+            dfs(treeNode.leftTree);
+            dfs(treeNode.rightTree);
+        }
+    }
+
+}
+
+class TreeNode{
+    int val;
+    TreeNode leftTree;
+    TreeNode rightTree;
+    TreeNode(int x){
+        val = x;
+    }
+}
