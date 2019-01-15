1ad334d274875462b490bfee93ddad3048409746
shanwei <shanwei0949@163.com>
2019-01-15 16:16:04 +0800

    27

---
+++ easyAlgorithm/src/RemoveElements.java
@@ -0,0 +1,17 @@
+/**
+ * 27 删除数组中相同的元素
+ */
+public class RemoveElements {
+
+    public int removeElement(int[] nums, int val) {
+        if (nums==null || nums.length==0) return 0;
+        int res= 0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=val){
+                nums[res++]=nums[i];
+            }
+        }
+        return res;
+    }
+
+}
